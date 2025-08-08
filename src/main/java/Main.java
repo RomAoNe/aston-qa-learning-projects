f192010d9fb292691d18600b56637873f2a8ad52
RomAoNe <roman130688@gmail.com>
2025-08-08 17:51:25 +0500

    Update Main.java with detailed greeting message

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is Lesson_1");
+        System.out.println("Hello, Git!" +
+                "\nThis is Lesson_1 of" +
+                " automation testing learning.");
     }
 }
