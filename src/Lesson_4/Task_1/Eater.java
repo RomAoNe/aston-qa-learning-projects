327cae61eefb179d9392296dcc1190ffd94d2ba7
RomAoNe <roman130688@gmail.com>
2025-08-26 14:03:30 +0500

    Выполнил ДЗ к Лекции 2.5

--- src/Lesson_4/Task_1/Eater.java
+++
@@ -1,7 +0,0 @@
-package Lesson_4.Task_1;
-
-public interface Eater {
-    boolean tryEat(Bowl bowl, int desiredFood);
-
-    void eat(Bowl bowl, int desiredFood);
-}
--- src/Lesson_4/Task_2/Area.java
+++
@@ -1,5 +0,0 @@
-package Lesson_4.Task_2;
-
-public interface Area {
-    double calculateArea();
-}
