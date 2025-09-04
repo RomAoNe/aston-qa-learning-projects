11cd426dbcab4922b99c80f47822f5704df7a2d1
RomAoNe <roman130688@gmail.com>
2025-09-05 00:27:28 +0500

    Выполнил ДЗ к Лекции 2.8

--- src/test/java/Lesson_8/RequestMethodsTest.java
+++ src/test/java/Lesson_8/RequestMethodsTest.java
@@ -2,7 +2,7 @@ package Lesson_8;
 
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
@@ -15,7 +15,7 @@ public class RequestMethodsTest {
     }
 
     @Test
-    @Disabled
+    @DisplayName("GET с query параметрами")
     public void getRequestWithQueryParams() {
         given()
                 .queryParam("foo1", "bar1")
@@ -32,7 +32,7 @@ public class RequestMethodsTest {
     }
 
     @Test
-    @Disabled
+    @DisplayName("POST с raw text")
     public void postRequestWithRawText() {
         String requestText = "{\n" +
                 "    \"test\": \"Hello world!\"\n" +
@@ -51,25 +51,58 @@ public class RequestMethodsTest {
     }
 
     @Test
+    @DisplayName("POST с form-data")
     public void postRequestWithFormData() {
+        given()
+                .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                .formParam("foo1", "bar1")
+                .formParam("foo2", "bar2")
 
+                .when()
+                .post("/post")
+
+                .then().log().body()
+                .statusCode(HttpStatus.SC_OK)
+                .body("form.foo1", equalTo("bar1"))
+                .body("form.foo2", equalTo("bar2"));
     }
 
     @Test
-    @Disabled
+    @DisplayName("PUT с текстовым телом")
     public void putRequest() {
-
+        given()
+                .contentType("text/plain")
+                .body("This is expected to be sent back as part of response body.")
+                .when()
+                .put("/put")
+                .then().log().body()
+                .statusCode(HttpStatus.SC_OK)
+                .body("data", equalTo("This is expected to be sent back as part of response body."));
     }
 
     @Test
-    @Disabled
+    @DisplayName("PATCH с текстовым телом")
     public void patchRequest() {
-
+        given()
+                .contentType("text/plain")
+                .body("This is expected to be sent back as part of response body.")
+                .when()
+                .patch("/patch")
+                .then().log().body()
+                .statusCode(HttpStatus.SC_OK)
+                .body("data", equalTo("This is expected to be sent back as part of response body."));
     }
 
     @Test
-    @Disabled
+    @DisplayName("DELETE с текстовым телом")
     public void deleteRequest() {
-
+        given()
+                .contentType("text/plain")
+                .body("This is expected to be sent back as part of response body.")
+                .when()
+                .delete("/delete")
+                .then().log().body()
+                .statusCode(HttpStatus.SC_OK)
+                .body("data", equalTo("This is expected to be sent back as part of response body."));
     }
 }
